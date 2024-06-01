84954cd79fc3a8241aa11f78548646b1d85e75a6
aliahsan676 <aliahsan676@gmail.com>
2024-06-01 17:40:54 +0600

    First commit

---
+++ src/test/java/com/api/test/ApiTest.java
@@ -0,0 +1,15 @@
+package com.api.test;
+
+public class ApiTest {
+
+    private static final String BASE_URL = "https://juice-shop.herokuapp.com";
+    private String token;
+    private String basketId;
+
+
+
+
+
+
+
+}
